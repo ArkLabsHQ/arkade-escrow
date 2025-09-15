f3129850a30f37cc02cd6454f9fe52881d4b3c6b
Pietro Grandi <pietro@bluelabs.eu>
2025-09-15 13:18:33 +0200

    debug SQLite access issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ConfigModule } from "@nestjs/config";
 import {
+	Logger,
 	MiddlewareConsumer,
 	Module,
 	NestModule,
@@ -18,17 +19,19 @@ import { RequestLoggingMiddleware } from "./common/middlewares/request-logging.m
 
 const isTest = process.env.NODE_ENV === "test";
 
+Logger.log(`Running in ${process.env.NODE_ENV} mode`);
+Logger.log(`SQLite DB path: ${process.env.SQLITE_DB_PATH}`);
+
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 		TypeOrmModule.forRootAsync({
 			useFactory: () => ({
 				type: "sqlite",
-				database: isTest
-					? ":memory:"
-					: (process.env.SQLITE_DB_PATH ?? "./data/ark-escrow.sqlite"),
+				database: isTest ? ":memory:" : process.env.SQLITE_DB_PATH,
 				entities: [User, EscrowRequest, EscrowContract],
 				synchronize: true,
+				logging: true,
 			}),
 		}),
 		AuthModule,
