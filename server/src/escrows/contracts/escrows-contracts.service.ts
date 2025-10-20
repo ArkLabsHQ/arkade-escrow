3c17d27382dc5c72fd56bc7c87b5abb46a1fd564
Pietro Grandi <pietro@bluelabs.eu>
2025-10-20 07:19:28 +0200

    fix

--- server/src/escrows/contracts/escrows-contracts.service.ts
+++ server/src/escrows/contracts/escrows-contracts.service.ts
@@ -14,7 +14,7 @@ import { Brackets, Repository } from "typeorm";
 import { nanoid } from "nanoid";
 import { randomUUID } from "node:crypto";
 import { EventEmitter2, OnEvent } from "@nestjs/event-emitter";
-import { ArkAddress /*, verifyTapscriptSignatures */ } from "@arkade-os/sdk";
+import { ArkAddress, verifyTapscriptSignatures } from "@arkade-os/sdk";
 import { Transaction } from "@scure/btc-signer";
 import { base64 } from "@scure/base";
 
@@ -453,9 +453,8 @@ export class EscrowsContractsService {
 		try {
 			this.logger.debug("verifying tapscript signatures");
 			const txBytes = base64.decode(signature.arkTx);
-			const _tx = Transaction.fromPSBT(txBytes, { allowUnknown: true });
-			// TODO: verify again
-			// verifyTapscriptSignatures(tx, 0, [signerPubKey]);
+			const tx = Transaction.fromPSBT(txBytes, { allowUnknown: true });
+			verifyTapscriptSignatures(tx, 0, [signerPubKey]);
 			this.logger.debug("OK tapscript signatures");
 		} catch (e) {
 			this.logger.error(e);
