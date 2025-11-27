4a471918dac8d22b16b182536ab8896b0940551d
Pietro Grandi <pietro@bluelabs.eu>
2025-11-27 12:35:22 +0100

    enable dark mode

--- backoffice/src/App.tsx
+++ backoffice/src/App.tsx
@@ -3,36 +3,39 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "next-themes";
+import Config from "@/Config.ts";
 import Home from "./pages/Home";
 import Contracts from "./pages/Contracts";
 import ContractDetails from "./pages/ContractDetails";
 import NotFound from "./pages/NotFound";
-import Config from "@/Config.ts";
 
 const queryClient = new QueryClient();
 
 const App = () => (
 	<QueryClientProvider client={queryClient}>
-		<TooltipProvider>
-			<Toaster />
-			<Toaster />
-			<Sonner />
-			<BrowserRouter>
-				<Routes>
-					<Route path={Config.appRootUrl} element={<Home />} />
-					<Route
-						path={`${Config.appRootUrl}/contracts`}
-						element={<Contracts />}
-					/>
-					<Route
-						path={`${Config.appRootUrl}/contracts/:externalId`}
-						element={<ContractDetails />}
-					/>
-					{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</TooltipProvider>
+		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+			<TooltipProvider>
+				<Toaster />
+				<Toaster />
+				<Sonner />
+				<BrowserRouter>
+					<Routes>
+						<Route path={Config.appRootUrl} element={<Home />} />
+						<Route
+							path={`${Config.appRootUrl}/contracts`}
+							element={<Contracts />}
+						/>
+						<Route
+							path={`${Config.appRootUrl}/contracts/:externalId`}
+							element={<ContractDetails />}
+						/>
+						{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</TooltipProvider>
+		</ThemeProvider>
 	</QueryClientProvider>
 );
 
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 import Requests from "./pages/Requests";
 import Contracts from "./pages/Contracts";
@@ -17,29 +18,31 @@ const App = () => (
 	<QueryClientProvider client={queryClient}>
 		<MessageProvider allowedChildOrigins={Config.hostUrls}>
 			<SessionProvider>
-				<TooltipProvider>
-					<Toaster />
-					<Sonner />
-					<BrowserRouter>
-						<Routes>
-							<Route path={Config.appRootUrl} element={<Index />} />
-							{/*<Route*/}
-							{/*	path={`${Config.appRootUrl}/notifications`}*/}
-							{/*	element={<Notifications />}*/}
-							{/*/>*/}
-							<Route
-								path={`${Config.appRootUrl}/requests`}
-								element={<Requests />}
-							/>
-							<Route
-								path={`${Config.appRootUrl}/contracts`}
-								element={<Contracts />}
-							/>
-							{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-							<Route path="*" element={<NotFound />} />
-						</Routes>
-					</BrowserRouter>
-				</TooltipProvider>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					<TooltipProvider>
+						<Toaster />
+						<Sonner />
+						<BrowserRouter>
+							<Routes>
+								<Route path={Config.appRootUrl} element={<Index />} />
+								{/*<Route*/}
+								{/*	path={`${Config.appRootUrl}/notifications`}*/}
+								{/*	element={<Notifications />}*/}
+								{/*/>*/}
+								<Route
+									path={`${Config.appRootUrl}/requests`}
+									element={<Requests />}
+								/>
+								<Route
+									path={`${Config.appRootUrl}/contracts`}
+									element={<Contracts />}
+								/>
+								{/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+								<Route path="*" element={<NotFound />} />
+							</Routes>
+						</BrowserRouter>
+					</TooltipProvider>
+				</ThemeProvider>
 			</SessionProvider>
 		</MessageProvider>
 	</QueryClientProvider>
